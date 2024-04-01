b815e761504e62fd8ab7c306f8acdaf5d5a8a013
veetiengblom <Veeti_engblom@hotmail.com>
2024-04-01 15:50:18 +0100

    Finished part 3.21

--- index.js
+++ index.js
@@ -6,21 +6,26 @@ const Person = require("./models/person");
 const app = express();
 
 app.use(express.static("dist"));
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 app.use(morgan("tiny"));
 
-app.get("/api/persons", (req, res) => {
+app.get("/api/persons", (req, res, next) => {
   console.log("Phonebook:");
-  Person.find({}).then((persons) => {
-    console.log(persons);
-    res.json(persons);
-  });
+  Person.find({})
+    .then((persons) => {
+      console.log(persons);
+      res.json(persons);
+    })
+    .catch((error) => next(error));
 });
 
-app.post("/api/persons", async (req, res) => {
-  console.log(req.body);
-  const { name, number } = req.body;
+app.post("/api/persons", async (req, res, next) => {
+  let { name, number } = req.body;
+
+  name = name.trim();
+  number = number.trim();
+
   if (!name || !number) {
     return res.status(400).json({
       error: "content missing",
@@ -36,38 +41,83 @@ app.post("/api/persons", async (req, res) => {
     name: name,
     number: number,
   });
-  console.log(person);
 
-  person.save().then((savedPerson) => {
-    console.log("person added");
-    res.json(savedPerson);
-  });
+  person
+    .save()
+    .then((savedPerson) => {
+      res.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 
-app.get("/info", (req, res) => {
-  Person.find({}).then((person) => {
-    const length = person.length;
-    const dateNow = new Date();
-    res.write(`Phonebook has info for ${length} people\n`);
-    res.write(`${dateNow}`);
-    res.end();
-  });
+app.get("/info", (req, res, next) => {
+  Person.find({})
+    .then((person) => {
+      const length = person.length;
+      const dateNow = new Date();
+      res.write(`Phonebook has info for ${length} people\n`);
+      res.write(`${dateNow}`);
+      res.end();
+    })
+    .catch((error) => next(error));
 });
 
-app.get("/api/persons/:id", (req, res) => {
+app.get("/api/persons/:id", (req, res, next) => {
   const id = req.params.id;
-  Person.findById(id).then((person) => {
-    res.json(person);
-  });
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (req, res) => {
+app.put("/api/persons/:id", (req, res, next) => {
   const id = req.params.id;
-  Person.deleteOne({ _id: id }).then((deletedPerson) => {
-    console.log("deleted", deletedPerson);
-    res.status(204).end();
-  });
+  const { name, number } = req.body;
+  if (number.trim() === "") {
+    return res.status(400).json({
+      error: "content missing",
+    });
+  }
+  const person = {
+    name: name,
+    number: number,
+  };
+  Person.findByIdAndUpdate(id, person, {
+    new: true,
+    runValidators: true,
+    context: "query",
+  })
+    .then((updatedPerson) => {
+      res.json(updatedPerson);
+    })
+    .catch((error) => next(error));
 });
+
+app.delete("/api/persons/:id", (req, res, next) => {
+  const id = req.params.id;
+  Person.findByIdAndDelete(id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+const errorHandler = (error, req, res, next) => {
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return res.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
--- models/person.js
+++ models/person.js
@@ -5,6 +5,26 @@ mongoose.set("strictQuery", false);
 //Muuta environment variableksi
 const url = process.env.MONGODB_URI;
 
+const numberValidator = (value) => {
+  const regex = /^[0-9]{2,3}-[0-9]{5,}$/;
+  //const regex = /^\d{2,3}-\d{5,}$/;
+  if (!regex.test(value)) {
+    return false;
+  }
+  // if (value.length < 8) {
+  //   return false;
+  // }
+  const parts = value.split("-");
+  const firstPart = parts[0];
+
+  // Check if the first part is 2-3 characters long
+  if (firstPart.length < 2 || firstPart.length > 3) {
+    return false;
+  }
+
+  return true;
+};
+
 mongoose
   .connect(url)
   .then((result) => {
@@ -16,8 +36,18 @@ mongoose
 
 const personSchema = new mongoose.Schema({
   id: String,
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minlength: 3,
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: numberValidator,
+      message:
+        "User input must be in correct format: for ex. 09-1234556, 040-22334455",
+    },
+  },
 });
 
 personSchema.set("toJSON", {
